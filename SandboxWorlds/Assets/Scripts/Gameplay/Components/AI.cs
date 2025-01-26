d8aab5a3ddd92d9c16daf3e27cb547ba0822ea6c
kana-meow <kana69lol@gmail.com>
2025-01-26 04:06:18 +0100

    Added Temporary String Variables For AI

--- SandboxWorlds/Assets/Scripts/Gameplay/Components/AI.cs
+++ SandboxWorlds/Assets/Scripts/Gameplay/Components/AI.cs
@@ -1,10 +1,33 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Base.Component {
 
     public class AI : _BaseComponent {
+        // both of these will be temprorarily stored as strings
+
+        [JsonProperty("behavior")]
+        public string _behavior;
+
+        public BehaviorType Behavior {
+            get {
+                return (BehaviorType)System.Enum.Parse(typeof(BehaviorType), _behavior);
+            }
+        }
+
+        [JsonProperty("goals")]
+        public List<string> goals;
 
         public override void OnInitialize() {
+        }
+
+        public enum BehaviorType {
+            None,
+            Passive,
+            Neutral,
+            Agressive
         }
     }
 }
