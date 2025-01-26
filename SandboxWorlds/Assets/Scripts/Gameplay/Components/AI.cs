3496f9225f7c4119ce647ed51adbf85bc5b2823b
kana-meow <kana69lol@gmail.com>
2025-01-26 15:42:17 +0100

    Made `behavior` use an enum with StringEnumConverter

--- SandboxWorlds/Assets/Scripts/Gameplay/Components/AI.cs
+++ SandboxWorlds/Assets/Scripts/Gameplay/Components/AI.cs
@@ -1,21 +1,17 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Base.Component {
 
     public class AI : _BaseComponent {
-        // both of these will be temprorarily stored as strings
 
         [JsonProperty("behavior")]
-        public string _behavior;
-
-        public BehaviorType Behavior {
-            get {
-                return (BehaviorType)System.Enum.Parse(typeof(BehaviorType), _behavior);
-            }
-        }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public BehaviorType behavior;
 
         [JsonProperty("goals")]
         public List<string> goals;
